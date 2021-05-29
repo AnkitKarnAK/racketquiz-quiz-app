fa64650ee157341dfe5fc8a97777ad067b7ee3c8
Ankit Karn <ankit.karn.ak@gmail.com>
2021-05-29 19:30:14 +0530

    added no quiz found component

--- src/components/pages/PlayQuiz.tsx
+++ src/components/pages/PlayQuiz.tsx
@@ -1,16 +1,19 @@
+// import { useState } from "react";
 import { useParams } from "react-router";
 import { useQuizData } from "../../context/QuizDataContext";
+import NoQuizFound from "../NoQuizFound";
 import QuizRules from "../QuizRules";
 
 const PlayQuiz = () => {
   const { quizName } = useParams();
   const { state } = useQuizData();
+  // const [isQuizStarted, setIsQuizStarted] = useState(false);
 
   const quizGame = state.categories.find(
     (category) => category.categoryName.toLowerCase() === quizName
   );
 
-  return <>{quizGame && <QuizRules quizGame={quizGame} />}</>;
+  return <>{quizGame ? <QuizRules quizGame={quizGame} /> : <NoQuizFound />}</>;
 };
 
 export default PlayQuiz;
