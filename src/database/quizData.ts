8f0131cb705a83ed413d5e6fde6493e5e4a2337a
Ankit Karn <ankit.karn.ak@gmail.com>
2021-06-06 16:09:43 +0530

    added intermediate & advance quiz data

--- src/database/quizData.ts
+++ src/database/quizData.ts
@@ -196,21 +196,27 @@ export const quizData: Quiz = {
     {
       _id: "kb37h8mfmy8",
       categoryName: "Intermediate",
-      totalQuestions: 2,
+      totalQuestions: 10,
       questions: [
         {
           _id: "nvmkcexpkl",
           questionNumber: 1,
-          question: "A shuttle tipped during play does count as a hit?",
+          question:
+            "British colonial officers brought a game like badminton back from",
           options: [
             {
               _id: "ftv5g1dbv7",
-              text: "Yes",
-              isRight: true,
+              text: "China",
+              isRight: false,
             },
             {
               _id: "t9xfyr1hmy8",
-              text: "No",
+              text: "India",
+              isRight: true,
+            },
+            {
+              _id: "xq53x7bsro",
+              text: "Japan",
               isRight: false,
             },
           ],
@@ -220,17 +226,216 @@ export const quizData: Quiz = {
         {
           _id: "1q01kajnpws",
           questionNumber: 2,
-          question: "Only one hit per side is allowed during play?",
+          question: "The word 'badminton' was originally the name of",
           options: [
             {
               _id: "7alufavafso",
-              text: "Yes",
-              isRight: true,
+              text: "an Indian bird",
+              isRight: false,
             },
             {
               _id: "wjmefwqrt7z",
-              text: "No",
+              text: "a British officer",
               isRight: false,
+            },
+            {
+              _id: "mhmefwqrv4g",
+              text: "a English House",
+              isRight: true,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "d40hkajnvns",
+          questionNumber: 3,
+          question:
+            "The world's first major badminton competition was held in England in",
+          options: [
+            {
+              _id: "vf4ufavavs3",
+              text: "1863",
+              isRight: false,
+            },
+            {
+              _id: "c4gefwqrtf4",
+              text: "1899",
+              isRight: true,
+            },
+            {
+              _id: "ngno4cn40",
+              text: "1903",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "1q01kajnpws",
+          questionNumber: 4,
+          question: "At its centre, a badminton net should be",
+          options: [
+            {
+              _id: "jk489cbcf",
+              text: "5ft high",
+              isRight: true,
+            },
+            {
+              _id: "sv89bd38d",
+              text: "6ft high",
+              isRight: false,
+            },
+            {
+              _id: "sgr9dn98ss",
+              text: "4ft high",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "1q01kajnpws",
+          questionNumber: 5,
+          question: "The game begins with",
+          options: [
+            {
+              _id: "s2efvaff44",
+              text: "an underhand serve",
+              isRight: true,
+            },
+            {
+              _id: "rhi83db88c",
+              text: "an overhead serve",
+              isRight: false,
+            },
+            {
+              _id: "3fjr89hf8n",
+              text: "a backhand serve",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "r8isfi9898",
+          questionNumber: 6,
+          question: "Hitting the shuttlecock into the net is a",
+          options: [
+            {
+              _id: "sf38h8b9",
+              text: "net shot",
+              isRight: false,
+            },
+            {
+              _id: "fsj3hidj9",
+              text: "duck",
+              isRight: false,
+            },
+            {
+              _id: "svj83bu9",
+              text: "fault",
+              isRight: true,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "csj73j777",
+          questionNumber: 7,
+          question: "To win, the margin in the score must be at least",
+          options: [
+            {
+              _id: "3hduiubu9",
+              text: "one points",
+              isRight: false,
+            },
+            {
+              _id: "fb489hdc89",
+              text: "two points",
+              isRight: true,
+            },
+            {
+              _id: "bdk876g77",
+              text: "three points",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "da7y3bd77nd",
+          questionNumber: 8,
+          question: "Before 1986, players could only win a point if they had",
+          options: [
+            {
+              _id: "fb78bwh7u",
+              text: "served",
+              isRight: true,
+            },
+            {
+              _id: "scnj83b8d",
+              text: "received",
+              isRight: false,
+            },
+            {
+              _id: "ed3df4dcd",
+              text: "faulted",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "efdh89y3bd8",
+          questionNumber: 9,
+          question: "Badminton was first played at the Olympic Games in",
+          options: [
+            {
+              _id: "d78sgu7x",
+              text: "Sydney in 2004",
+              isRight: false,
+            },
+            {
+              _id: "dki78dg37d",
+              text: "Seoul in 1988",
+              isRight: false,
+            },
+            {
+              _id: "dcnj3h3s2",
+              text: "Barcelona in 1992",
+              isRight: true,
+            },
+          ],
+          points: 5,
+          negativePoints: 2,
+        },
+        {
+          _id: "3javufu83x9as",
+          questionNumber: 10,
+          question:
+            "Fu Haifeng's 2005 speed record for hitting the shuttlecock was",
+          options: [
+            {
+              _id: "dvsiu237dd",
+              text: "233 km/h",
+              isRight: false,
+            },
+            {
+              _id: "cai873jsk0",
+              text: "283Km/h",
+              isRight: false,
+            },
+            {
+              _id: "aj972bhj",
+              text: "332 km/h",
+              isRight: true,
             },
           ],
           points: 5,
@@ -241,21 +446,32 @@ export const quizData: Quiz = {
     {
       _id: "d337h8mfgg5",
       categoryName: "Advance",
-      totalQuestions: 2,
+      totalQuestions: 10,
       questions: [
         {
           _id: "sdmkcdxgf3",
           questionNumber: 1,
-          question: "A shuttle tipped during play does count as a hit?",
+          question:
+            "Which badminton player won the Indian Open Grand Prix Gold championship in 2014?",
           options: [
             {
               _id: "fvv5g1dbc3",
-              text: "Yes",
-              isRight: true,
+              text: "Misha Zilberman",
+              isRight: false,
             },
             {
               _id: "3cxfyr1hmcv",
-              text: "No",
+              text: "Xue Song",
+              isRight: true,
+            },
+            {
+              _id: "djd78bd38",
+              text: "Kashyap Parupalli",
+              isRight: false,
+            },
+            {
+              _id: "dnu7gc7db",
+              text: "Brice Leverdez",
               isRight: false,
             },
           ],
@@ -265,16 +481,265 @@ export const quizData: Quiz = {
         {
           _id: "3cd0kajnpce",
           questionNumber: 2,
-          question: "Only one hit per side is allowed during play?",
+          question:
+            "In which stadium India Open Grand Prix Gold tournament held?",
           options: [
             {
               _id: "3vlufavav4",
-              text: "Yes",
+              text: "Babu Banarasi Das Indoor Stadium",
               isRight: true,
             },
             {
               _id: "sfmefwqrv4x",
-              text: "No",
+              text: "Rajiv Gandhi Indoor Stadium",
+              isRight: false,
+            },
+            {
+              _id: "afj99h3ss",
+              text: "Sports Authority of India Complex",
+              isRight: false,
+            },
+            {
+              _id: "dsi37d87ib",
+              text: "All of these",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "2g873b87djks",
+          questionNumber: 3,
+          question:
+            "Which player won the women’s singles title at India Open Super Series Badminton tournament 2015?",
+          options: [
+            {
+              _id: "dckj3873u",
+              text: "Saina Nehwal",
+              isRight: true,
+            },
+            {
+              _id: "diu387gss",
+              text: "Jwala Gutta",
+              isRight: false,
+            },
+            {
+              _id: "de98diu3d",
+              text: "Yihan Wang",
+              isRight: false,
+            },
+            {
+              _id: "alkn983sd",
+              text: "None of these",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "s3983bid9k",
+          questionNumber: 4,
+          question: "Who won India's first Olympic Medal in badminton?",
+          options: [
+            {
+              _id: "dsu387td3s",
+              text: "Prakash Padukone",
+              isRight: false,
+            },
+            {
+              _id: "sfmefwqrv4x",
+              text: "Saina Nehwal",
+              isRight: true,
+            },
+            {
+              _id: "dscs893bka",
+              text: "P Gopichand",
+              isRight: false,
+            },
+            {
+              _id: "dc2o389ysa",
+              text: "PV Sindhu",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "d398ys9bjk89",
+          questionNumber: 5,
+          question:
+            "Who is the youngest badminton player to represent India at championships?",
+          options: [
+            {
+              _id: "c38eybkds",
+              text: "Vaishnavi Reddy",
+              isRight: false,
+            },
+            {
+              _id: "skoh3bkx",
+              text: "Gayatri Gopichand",
+              isRight: true,
+            },
+            {
+              _id: "sh3ydhz",
+              text: "Radhika Menon",
+              isRight: false,
+            },
+            {
+              _id: "dslndoho",
+              text: "Jwala Gutta",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "s3jgiydh3o0",
+          questionNumber: 6,
+          question:
+            "Who became the first Indian to win the All England Open Championship in 1980?",
+          options: [
+            {
+              _id: "skjii3ds",
+              text: "Prakash Padukone",
+              isRight: true,
+            },
+            {
+              _id: "do398ydos",
+              text: "Chetan Anand",
+              isRight: false,
+            },
+            {
+              _id: "sdk398yde",
+              text: "Rajeev Bagga",
+              isRight: false,
+            },
+            {
+              _id: "=sjui73dds",
+              text: "Pranav Chopra",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "wdsh93ndw3d",
+          questionNumber: 7,
+          question:
+            "Who was the first Indian badminton player to hold number one rank in World rankings?",
+          options: [
+            {
+              _id: "dslkuose3",
+              text: "Manu Attri",
+              isRight: false,
+            },
+            {
+              _id: "aiha8y2sjj",
+              text: "Chetan Anand",
+              isRight: false,
+            },
+            {
+              _id: "skn32ys93",
+              text: "Prakash Padukone",
+              isRight: true,
+            },
+            {
+              _id: "sd38dnf",
+              text: "None of these",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "dskjb387i3x",
+          questionNumber: 8,
+          question:
+            "Who is the first Indian badminton player to won the Gold Medal at the Commonwealth Games in Australia?",
+          options: [
+            {
+              _id: "dsj37sui",
+              text: "Syed Modi",
+              isRight: true,
+            },
+            {
+              _id: "akj397a",
+              text: "Gayatri Gopichand",
+              isRight: false,
+            },
+            {
+              _id: "adj27wuib2",
+              text: "Prakash Padukone",
+              isRight: false,
+            },
+            {
+              _id: "dsj3iusi",
+              text: "Pullela Gopichand",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "dsklndejk33",
+          questionNumber: 9,
+          question: "Who won the All England Open Championship in 2001?",
+          options: [
+            {
+              _id: "sdu398hod",
+              text: "Gurusai Dutt",
+              isRight: false,
+            },
+            {
+              _id: "efkn3uy3d",
+              text: "Pullela Gopichand",
+              isRight: true,
+            },
+            {
+              _id: "abjalpou9s",
+              text: "Prakash Padukone",
+              isRight: false,
+            },
+            {
+              _id: "dskjb87kj",
+              text: "Kidambi Srikanth",
+              isRight: false,
+            },
+          ],
+          points: 5,
+          negativePoints: 3,
+        },
+        {
+          _id: "zdj398nd8oih",
+          questionNumber: 10,
+          question:
+            "The female badminton player became the youngest Padma Shri awardee in 2015. Who is she?",
+          options: [
+            {
+              _id: "so3hhdolj",
+              text: "Jwala Gutta",
+              isRight: false,
+            },
+            {
+              _id: "sj3hyd93d",
+              text: "Saina Nehwal",
+              isRight: false,
+            },
+            {
+              _id: "sdkj3y9dbk",
+              text: "PV Sindhu",
+              isRight: true,
+            },
+            {
+              _id: "sdjh89hjj",
+              text: "Vaishnavi Reddy",
               isRight: false,
             },
           ],
